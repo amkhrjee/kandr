1679a40f764a8cb9dbe4c288480e79efb11757b1
amkhrjee <aniruddha.mukherji@outlook.com>
2022-06-01 00:33:49 +0530

    pol_cal

--- chap_four/pol_cal.c
+++ chap_four/pol_cal.c
@@ -3,6 +3,10 @@
 
 #define MAXOP 100
 #define NUMBER '0'
+#define MAXVAL 100
+
+int sp = 0;
+double val[MAXVAL]; 
 
 int getop(char []);
 void push(double);
@@ -16,6 +20,47 @@ int main(){
 	while((type = getop(s)) != EOF){
 		switch(type){
 		case NUMBER: 
-		
+			push(atof(s)); 
+			break; 
+		case '+'; 
+			push(pop() + pop()); 
+			break; 
+		case '*': 
+			push(pop()*pop());
+			break;
+		case '-': 
+			op2 = pop(); 
+			push(pop() - op2);
+			break; 
+		case '/':
+			op2 = pop();
+			if(op2 != 0.0)
+				push(pop() / op2);
+			else
+				printf("error: zero divisor\n");
+			break;
+		case '\n':
+			printf("\t%.8g\n", pop()); 
+			break; 
+		default: 
+			printf("error: unknown command\n"); 
+			break; 
+		}
+	}
+	return 0;
+}
+void push(double f){
+	if(sp < MAXVAL)
+		val[sp++] = f;
+	else
+		printf("error: stack full, can't push %g\n", f); 
 }
 
+double pop(void){
+	if(sp > 0)
+		return val[--sp]; 
+	else{
+		printf("error: stack empty\n"); 
+		return 0.0; 
+	}
+}
